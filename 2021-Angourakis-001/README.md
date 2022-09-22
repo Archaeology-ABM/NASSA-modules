80b47c52c7daceeb73015b91014bcdac03d6bce5
Andreas Angourakis <andros.spica@gmail.com>
2022-09-23 00:08:08 +0200

    Update 2021-Angourakis-001
    - minor edits in NASSA.yml, README.md
    - randomWalk_v01.py: edits to comments and improved tests
    - demonstration.ipynb: improvements in markdown text and a few code chunks

--- 2021-Angourakis-001/README.md
+++ 2021-Angourakis-001/README.md
@@ -1,13 +1,15 @@
 # Random walk in 2D space based on agent angle orientation
 *by Andreas Angourakis* (NASSA submission :rocket:)
 
-An implementation of random walk in Python, which defines an agent class `Walker` capable of several movement algorithms based on angle and movement step distance in a continuous 2D space.
+An implementation of random walk in Python, which defines an agent class `Walker` capable of random walk movement based on random angle and movement step distance in a continuous 2D space.
 
 ## License
 
 MIT
 
 ## Further information
+
+![Example of output: three random walk trajectories in continuous 2D space](python_implementation\documentation\randomWalk_v01_plot.png)
 
 [`demonstration.ipynb`](python_implementation/demonstration.ipynb): Jupyter Notebook using the Python implementation that sets up a workflow for executing several random walks and plotting them. It also includes a demonstration of the test methods.
 
--- 2021-Angourakis-001/python_implementation/randomWalk_v01.py
+++ 2021-Angourakis-001/python_implementation/randomWalk_v01.py
@@ -43,18 +43,18 @@ class Walker:
         and limited to given world dimensions. Uses uniform distributions for random values. 
         Sets Walker instance new position and returns new x, y, and direction in degrees.
         '''
-        # get new position
+        ### get new position
         x,y,newDirection = self.MoveRandomFree(angleLeftMax, angleRightMax,
                              moveDistanceMin, moveDistanceMax)
-        # assure that new position is within world dimensions
+        ### assure that new position is within world dimensions
         while self.IsOutsideWorld(x, y, worldDimensions):
             x,y,newDirection = self.MoveRandomFree(angleLeftMax, angleRightMax,
                                  moveDistanceMin, moveDistanceMax)
-        # set new position
+        ### set new position
         self.positionX = x
         self.positionY = y
         self.direction = newDirection
-        # return position coordinates and direction (display or testing purposes)
+        ### return position coordinates and direction (display or testing purposes)
         return x,y,newDirection
 
     def MoveRandomFree(self,
@@ -66,16 +66,16 @@ class Walker:
         2D random walk with parameters for direction and distance variation. 
         Uses uniform distributions for random values. Returns new x, y, and direction in degrees.
         '''
-        # set new random direction
+        ### set new random direction
         newDirection = self.GetRandomRotation(self.direction, angleLeftMax, angleRightMax)
-        # convert angle to radians
+        ### convert angle to radians
         directionInRadians = newDirection * math.pi / 180
-        # get movement distance
+        ### get movement distance
         moveDist = moveDistanceMin + (moveDistanceMax - moveDistanceMin) * random.uniform(0,1)
-        # get new position
+        ### get new position
         x = math.cos(directionInRadians) * moveDist
         y = math.sin(directionInRadians) * moveDist
-        # return position coordinates and direction
+        ### return position coordinates and direction
         return x,y,newDirection
     
     def GetRandomRotation(self,
@@ -87,11 +87,11 @@ class Walker:
         (currentDirection - angleLeftMax, currentDirection + angleRightMax). 
         Uses uniform distributions for random values. Returns new direction in degrees.
         '''
-        # get new random direction from previous direction 
-        # within the range -angleLeftMax, +angleRightMax
+        ### get new random direction from previous direction 
+        ### within the range -angleLeftMax, +angleRightMax
         newDirection = random.uniform(currentDirection - angleLeftMax, 
                                       currentDirection + angleRightMax)
-        # convert negative angle to positive
+        ### convert negative angle to positive
         if newDirection < 0.0:
            newDirection += 360
         return newDirection
@@ -107,28 +107,56 @@ class Walker:
         '''
         Checks if the coordinates are outside the world dimensions. Returns Boolean (true/false). 
         '''
-        # create class instance
+        ### create class instance
         randomWalker = Walker(x, y, 0.0)
 
         return ("IsOutsideWorld("
                + "x = " + str(x) 
                + ", y = " + str(y) 
                + ", worldDimensions = " + str(worldDimensions)
-               + ") returns "
+               + ") -> "
                + str(randomWalker.IsOutsideWorld(x, y, worldDimensions)))
     
     Test_IsOutsideWorld = staticmethod(Test_IsOutsideWorld)
 
     def Test_GetRandomRotation(currentDirection, angleLeftMax = 180, angleRightMax = 180):
-        # create class instance
+        '''
+        Checks if GetRandomRotation() returns an angle within the given specifications. Returns message ending with Boolean (true/false). 
+        '''
+        ### create class instance
         randomWalker = Walker(0.0, 0.0, currentDirection)
+
+        ### find left and right angle limits
+        rightLimit = (currentDirection + angleRightMax)
+        if rightLimit > 360: rightLimit = rightLimit - 360
+        leftLimit = (currentDirection - angleLeftMax)
+        if leftLimit < 0: leftLimit = 360 + leftLimit
+        
+        ### get new direction
+        newDirection = randomWalker.GetRandomRotation(currentDirection, angleLeftMax, angleRightMax)
+        
+        ### test
+        passTest = False
+        ### case 1: the whole range is greater than 0 and lower than 360
+        if (newDirection >= leftLimit) & (newDirection <= rightLimit):
+            passTest = True
+        ### other cases, the range overlaps with the limit 360-0
+        ### case 2: newDirection falls on the left of 0-360
+        elif (newDirection >= leftLimit) & (newDirection <= 360 + rightLimit):
+            passTest = True
+        ### case 3: newDirection falls on the right of 0-360
+        elif (newDirection >= leftLimit - 360) & (newDirection <= rightLimit):
+            passTest = True
+        ### case 4: newDirection equals 0-360
+        elif ((newDirection == 0) | (newDirection == 360)) & ((leftLimit <= 360) & (rightLimit >= 0)):
+            passTest = True
 
         return ("GetRandomRotation("
                 + "currentDirection = " + str(currentDirection)
                 + ", angleLeftMax = " + str(angleLeftMax)
                 + ", angleRightMax = " + str(angleRightMax)
-                + ") returns newDirection = "
-                + str(randomWalker.GetRandomRotation(currentDirection, angleLeftMax, angleRightMax)))
+                + ") -> newDirection = "
+                + str(newDirection) + " | pass test = " + str(passTest))
     
     Test_GetRandomRotation = staticmethod(Test_GetRandomRotation)
 
@@ -137,7 +165,10 @@ class Walker:
                             angleRightMax = 180, 
                             moveDistanceMin = 1.0, 
                             moveDistanceMax = 1.0):
-        # create class instance
+        '''
+        Checks if MoveRandomFree() returns a position and direction within the given specifications. Returns message. 
+        '''
+        ### create class instance
         randomWalker = Walker(x, y, currentDirection)
         
         return ("Walker("
@@ -149,7 +180,7 @@ class Walker:
                 + ", angleRightMax = " + str(angleRightMax)
                 + ", moveDistanceMin = " + str(moveDistanceMin)
                 + ", moveDistanceMax = " + str(moveDistanceMax)
-                + ") returns (x,y,newDirection) = "
+                + ") -> (x,y,newDirection) = "
                 + str(randomWalker.MoveRandomFree(angleLeftMax, angleRightMax, moveDistanceMin, moveDistanceMax)))
 
     Test_MoveRandomFree = staticmethod(Test_MoveRandomFree)
@@ -160,7 +191,10 @@ class Walker:
                         moveDistanceMin = 1.0, 
                         moveDistanceMax = 1.0,
                         worldDimensions = {'x':(-50,50), 'y':(-50,50)}):
-        # create class instance
+        '''
+        Checks if MoveRandom() returns a position and direction within the given specifications. Returns message. 
+        '''
+        ### create class instance
         randomWalker = Walker(x, y, currentDirection)
         
         return ("Walker("
